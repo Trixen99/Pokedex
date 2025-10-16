680ee9e16cafb3ddcebeea85d3e1da22c3467f92
Trixen99 <NathanWhitehead99@hotmail.com>
2025-10-16 22:29:41 +0100

    attempts to fix help function

--- commands.go
+++ commands.go
@@ -5,6 +5,10 @@ import (
 	"os"
 )
 
+type commandStart interface {
+	startcommand()
+}
+
 type cliCommand struct {
 	name        string
 	description string
@@ -17,10 +21,28 @@ var validCommands = map[string]cliCommand{
 		description: "Exit the Pokedex",
 		callback:    commandExit,
 	},
+	"help": {
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandExit,
+	},
 }
+
+//func (c cliCommand) startCommand {
+
+//}
 
 func commandExit() error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return fmt.Errorf("Status code: 0")
 }
+
+/*func commandHelp() error {
+	fmt.Println("Usage:")
+	for _, command := range validCommands {
+		fmt.Printf("%v: %v", command.name, command.description)
+	}
+	return nil
+}
+*/
